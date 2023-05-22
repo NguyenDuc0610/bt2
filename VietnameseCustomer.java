6075f268a106b5aa6f966a405078c13fd80ca794
NguyenDuc0610 <119928685+NguyenDuc0610@users.noreply.github.com>
2023-05-22 16:57:39 +0700

    Update and rename VietnameseCustomer.java to HoaDon.java

--- VietnameseCustomer.java
+++
@@ -1,6 +0,0 @@
-class VietnameseCustomer extends Customer {
-
-    public VietnameseCustomer(String id, String name, String date, String type, double quantity, double unitPrice, double threshold) {
-        super(id, name, date, type, quantity, unitPrice);
-    }
-}
